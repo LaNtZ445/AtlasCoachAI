a8b56e7cea3f43b854af85a1dea1de57317ad3db
LaNtZ445 <jacoblimparlantz@gmail.com>
2025-01-01 22:27:37 +0100

    added link to loog

--- AtlasCoachAI/src/components/Header/Header.tsx
+++ AtlasCoachAI/src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { StyledHeader, StyledNav, StyledLink, StyledLogo } from "./styles";
 
 function Header() {
   return (
     <StyledHeader>
-      <StyledLogo>AtlasCoach AI</StyledLogo>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <StyledLogo>AtlasCoach AI</StyledLogo>
+      </Link>
       <StyledNav>
         <StyledLink href="/workouts">Workouts</StyledLink>
         <StyledLink href="/meal-plans">Meal Plans</StyledLink>
